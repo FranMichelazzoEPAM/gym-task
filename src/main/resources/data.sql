ffc324f1fc5609b17107054c9f1387deb4245897
Francisco Michelazzo <francisco_michelazzo@epam.com>
2026-09-14 10:40:21 -0300

    Replace old HashMap initialization with newer data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO training_types (id, training_type_name) VALUES
+                                                        (RANDOM_UUID(), 'Cardio'),
+                                                        (RANDOM_UUID(), 'Strength'),
+                                                        (RANDOM_UUID(), 'Yoga'),
+                                                        (RANDOM_UUID(), 'Pilates'),
+                                                        (RANDOM_UUID(), 'CrossFit');
